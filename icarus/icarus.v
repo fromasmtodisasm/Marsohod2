b1584a1376e63bb12068307795accb827d6c777d
HFoxtail <hplus.foxtail@gmail.com>
2017-08-08 12:50:54 +0300

    add new gitignore, fix tabs

--- icarus/icarus.v
+++ icarus/icarus.v
@@ -2,25 +2,77 @@
 
 module main;
 
+// --------------------------------------------------------------------------
+// ИНИЦИАЛИЗАЦИЯ ВВОДА-ВЫВОДА
+// --------------------------------------------------------------------------
+
 reg clk;
-reg pll_0;
-reg pll_1;
-reg pll_2;
+reg clock_25;
+reg clock_12;
+reg clock_6;
+
+// Базовые
+reg [3:0]   led;  // Out
+reg [1:0]   keys; // In
+
+// ADC
+reg         adc_clock_20mhz;
+reg [7:0]   adc_input;
+
+// SDRAM
+reg        sdram_clock;
+reg [11:0] sdram_addr;
+reg [1:0]  sdram_bank;
+reg [15:0] sdram_dq;        // InOut
+reg        sdram_ldqm;
+reg        sdram_udqm;
+reg        sdram_ras;
+reg        sdram_cas;
+reg        sdram_we;
+    
+// VGA
+reg [4:0]  vga_red;
+reg [5:0]  vga_green;
+reg [4:0]  vga_blue;
+reg        vga_hs;
+reg        vga_vs;
+
+// FTDI (PORT-B)
+reg        ftdi_rx;
+reg        ftdi_tx;
+
+/* ----------------
+* Шилд расширения
+* ---------------- */
+
+// USB-A    2 pins
+reg [1:0]  usb;
+
+// SOUND    2 channel
+reg        sound_left;
+reg        sound_right;
+
+// PS/2     keyb / mouse
+reg [1:0]  ps2_keyb;
+reg [1:0]  ps2_mouse;
 
 // Моделируем сигнал тактовой частоты
-always #0.5 clk   = ~clk;
-always #4   pll_0 = ~pll_0;
-always #8   pll_1 = ~pll_1;
-always #16  pll_2 = ~pll_2;
+always #0.5 clk         = ~clk;      // 100.00
+always #4   clock_25    = ~clock_25; //  25.00
+always #8   clock_12    = ~clock_12; //  12.00
+always #16  clock_6     = ~clock_6;  //   6.25
 
 // От начала времени...
 initial begin
-  clk = 1;
-  pll_0 = 0;
-  pll_1 = 0;
-  pll_2 = 0;
+
+  clk      = 1;
+  clock_25 = 0;
+  clock_12 = 0;
+  clock_6  = 0;
   #2000 $finish;
+
 end 
+// --------------------------------------------------------------------------
 
 // создаем файл VCD для последующего анализа сигналов
 initial
@@ -31,11 +83,11 @@ begin
     
 end
 
-// -----------------------------------
+// --------------------------------------------------------------------------
+// ПРОГРАММИРУЕМЫЙ КОМПЛЕКС МОДУЛЕЙ ДЛЯ ТЕСТБЕНЧА
+// --------------------------------------------------------------------------
 
 // ПРИМЕР
 // cpu CPU(clk, i_data, o_data, o_addr);
-
-
 
 endmodule
