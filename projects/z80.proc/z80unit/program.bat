59e2e61e39676f0d426d96c9d385fc2406240802
HFoxtail <hplus.foxtail@gmail.com>
2018-01-14 17:39:54 +0300

    add fonts

--- projects/z80.proc/z80unit/program.bat
+++ projects/z80.proc/z80unit/program.bat
@@ -2,6 +2,7 @@ zmac unit.asm
 
 dd if=/dev/random of=rom.bin bs=1 count=32768
 dd if=/dev/zero of=rom.bin bs=1 count=16384
+dd if=../resources/fonts8x8.bin bs=1 skip=256 seek=15616
 dd if=zout/unit.cim of=rom.bin bs=1
 
 mode com9 baud=460800 data=8
