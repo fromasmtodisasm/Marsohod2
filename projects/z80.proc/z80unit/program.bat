49afe546a413871c97bcac888960a883e6f3d348
Foxtail <hplus.foxtail@gmail.com>
2018-01-14 15:20:04 +0700

    fix assembler

--- projects/z80.proc/z80unit/program.bat
+++ projects/z80.proc/z80unit/program.bat
@@ -1,6 +1,8 @@
+zmac unit.asm
+
 dd if=/dev/random of=rom.bin bs=1 count=32768
 dd if=/dev/zero of=rom.bin bs=1 count=16384
-dd if=unit.bin of=rom.bin bs=1
+dd if=zout/unit.cim of=rom.bin bs=1
 
 mode com9 baud=460800 data=8
 copy rom.bin /b com9
