6f2a8fc9b6dd298285cb31bb19dc7e54e6e5d39a
HFoxtail <hplus.foxtail@gmail.com>
2018-05-02 18:14:52 +0300

    add multest

---
+++ projects/06_multest/marsohod2.v
@@ -0,0 +1,73 @@
+module marsohod2(
+
+    /* ----------------
+     * Archectural Marsohod2
+     * ---------------- */
+
+    // CLOCK    100 Mhz
+    input   wire        clk,
+
+    // LED      4
+    output  wire [3:0]  led,
+
+    // KEYS     2
+    input   wire [1:0]  keys,
+
+    // ADC      8 bit
+    output  wire        adc_clock_20mhz,
+    input   wire [7:0]  adc_input,
+
+    // SDRAM
+    output  wire        sdram_clock,
+    output  wire [11:0] sdram_addr,
+    output  wire [1:0]  sdram_bank,
+    inout   wire [15:0] sdram_dq,
+    output  wire        sdram_ldqm,
+    output  wire        sdram_udqm,
+    output  wire        sdram_ras,
+    output  wire        sdram_cas,
+    output  wire        sdram_we,
+
+    // VGA
+    output  wire [4:0]  vga_red,
+    output  wire [5:0]  vga_green,
+    output  wire [4:0]  vga_blue,
+    output  wire        vga_hs,
+    output  wire        vga_vs,
+
+    // FTDI (PORT-B)
+    input   wire        ftdi_rx,
+    output  wire        ftdi_tx,
+
+    /* ----------------
+     * Extension Shield
+     * ---------------- */
+
+    // USB-A    2 pins
+    inout   wire [1:0]  usb,
+
+    // SOUND    2 channel
+    output  wire        sound_left,
+    output  wire        sound_right,
+
+    // PS/2     keyb / mouse
+    inout   wire [1:0]  ps2_keyb,
+    inout   wire [1:0]  ps2_mouse
+);
+// --------------------------------------------------------------------------
+
+reg [127:0] S = 128'hF123456789ABCDEF;
+
+always @(posedge clk) S <= S + 1'b1;
+
+wire [255:0] Rx = S[127:64] * S[63:0];
+// ---
+wire [191:0] Rb = Rx[255:128] ^ Rx[127:0];
+wire [95:0]  Rc = Rb[127:64]  ^ Rb[63:0];
+wire [47:0]  Rd = Rc[63:32]   ^ Rc[31:0];
+wire [23:0]  Re = Rd[31:16]   ^ Rd[15:0];
+wire [11:0]  R  = Re[15:8]    ^ Re[7:0];
+
+assign sdram_addr = R[11:0];
+
+endmodule
