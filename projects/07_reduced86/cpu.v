d95f90581f6073e7464318f285b1c113c3025bf9
HFoxtail <hplus.foxtail@gmail.com>
2018-05-02 20:23:38 +0300

    cpuator

--- projects/07_reduced86/cpu.v
+++ projects/07_reduced86/cpu.v
@@ -2,13 +2,41 @@ module cpu(
 
     /* Стандартные входы-выходы */
     input   wire        clk,        // 100 мегагерц
-    input   wire [15:0] i,          // Data In (16 бит)
-    output  reg  [15:0] o,          // Data Out,
-    output  wire [31:0] a,          // Address 32 bit
-    output  reg         w           // Write Enabled
+    input   wire [7:0]  i,          // Data In (16 бит)
+    output  reg  [7:0]  o,          // Data Out,
+    output  wire [19:0] a,          // Address 32 bit
+    output  reg         wm,         // Запись [d] на переднем фронте
+    output  reg  [7:0]  d,          // То, что записываем в память
+    
+    /* Регистровый файл */
+    output  reg  [2:0]  Dr,         // Номера регистров A, B
+    output  reg  [2:0]  Sr,
+    output  wire [1:0]  b,          // Битность 0 - 8 bit, 1-16, 2-32, 3-?
+    input   wire [15:0] ia,         // Значение регистра A
+    input   wire [15:0] ib,         // Значение регистра B
+    output  reg         wr,         // Запись в регистр
+    output  reg  [15:0] dw          // Значение для записи (номер регистра: A)
 );
 
 // Реализация делителя частоты 1/4, т.е. выдача 25 Мгц
-reg [1:0] d = 2'b00; always @(posedge clk) d <= d + 1;
+reg [1:0] D = 2'b00; assign clk25 = D[1]; always @(posedge clk) D <= D + 1;
+
+initial begin
+
+    dw = 16'h0000;
+    d  = 16'h0000;
+    o  = 8'h00;
+    wr = 1'b0;
+    wm = 1'b0;
+    Dr = 3'h0;
+    Sr = 3'h0;
+
+end
+
+// Главные такты
+always @(posedge clk25) begin
+
+
+end
 
 endmodule
--- projects/07_reduced86/ic.v
+++ projects/07_reduced86/ic.v
@@ -12,11 +12,46 @@ initial begin $dumpfile("result.vcd"); $dumpvars(0, main); end
 
 // ---------------------------------------------------------------------
 
-wire [15:0] i;
-wire [15:0] o;
-wire [31:0] a;
+wire [7:0]  i; wire [7:0] o; wire [19:0] a;
 wire        w;
+wire [7:0]  d; 
+
+// ------------------------------------- Регистровый файл --------------
+
+wire [2:0]  Dr;  wire [2:0] Sr; wire [1:0]  b;
+reg  [15:0] ia; reg  [15:0] ib;
+wire        wr; wire [15:0] dw;
+
+// Список регистров
+reg [31:0] registers[8];
+
+initial begin
+
+    registers[0] = 16'h0000; // AX
+    registers[1] = 16'h0000; // CX
+    registers[2] = 16'h0000; // DX
+    registers[3] = 16'h0000; // BX
+    registers[4] = 16'h0000; // SP
+    registers[5] = 16'h0000; // BP
+    registers[6] = 16'h0000; // SI 
+    registers[7] = 16'h0000; // DI
     
-cpu CPU(clk, i, o, a, w);
+    ia = 16'h0000;
+    ib = 16'h0000;
+    
+end
+
+always @(posedge clk) begin
+
+    ia <= registers[ Dr ];
+    ib <= registers[ Sr ];
+    
+    if (wr) registers[ Dr ] <= dw;
+
+end
+
+// ------------------------------------- Центральный процессор ---------
+cpu CPU(/* Главное */   clk, i, o, a, w, d, 
+        /* Регистры */  Dr, Sr, b, ia, ib, wr, dw);
     
 endmodule
