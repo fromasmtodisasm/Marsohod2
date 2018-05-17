3a359b0a172ed1ddd5f4f592a5ef30411315d2d6
HFoxtail <hplus.foxtail@gmail.com>
2018-05-17 15:01:33 +0300

    convert at -> xt

--- projects/07_reduced86/port_controller.v
+++ projects/07_reduced86/port_controller.v
@@ -13,7 +13,7 @@ module port_controller(
     input  wire         ps2_data_clk
     
 );
-    
+
 // Роутер
 // ---------------------------------------------------------------------
 
@@ -29,6 +29,7 @@ always @* begin
 end
 
 // ---------------------------------------------------------------------
+// Скан-коды: https://ru.wikipedia.org/wiki/Скан-код
 
 /* Обработчик клавиатуры */
 reg [7:0] keyb_char    = 8'h00;  /* Последний принятый байт */
@@ -37,15 +38,68 @@ reg       keyb_ready2  = 1'b0;   /* Acknowlegde */
 wire      keyb_ready   = keyb_ready1 ^ keyb_ready2; /* Бит 0 порта 64h */
 reg [1:0] keyb_jread   = 2'b00;     /* Признак только что принятого байта из порта 60h */
 reg [7:0] keyb_data    = 8'h0;     /* Выходные данные для порта */
-reg [7:0] keyb_data_xt = 8'h0;     /* Сконвертированное AT -> XT */
+reg [7:0] keyb_xt      = 8'h0;     /* Сконвертированное AT -> XT */
 reg       keyb_unpressed = 1'b0;  /* Признак "отжатой" клаваши */
 
 always @(*) begin
 
     case (ps2_data)
     
-        /* ESC */ 8'h76: keyb_data_xt = 8'h01;
-        default: keyb_data_xt = ps2_data;
+        /* ESC */ 8'h76: keyb_xt = 8'h01;
+        /* A   */ 8'h1C: keyb_xt = 8'h1E;
+        /* B   */ 8'h32: keyb_xt = 8'h30;
+        /* C   */ 8'h21: keyb_xt = 8'h2E;
+        /* D   */ 8'h23: keyb_xt = 8'h20;
+        /* E   */ 8'h24: keyb_xt = 8'h12;
+        /* F   */ 8'h2B: keyb_xt = 8'h21;
+        /* G   */ 8'h34: keyb_xt = 8'h22;
+        /* H   */ 8'h33: keyb_xt = 8'h23;
+        /* I   */ 8'h43: keyb_xt = 8'h17;
+        /* J   */ 8'h3B: keyb_xt = 8'h24;
+        /* K   */ 8'h42: keyb_xt = 8'h25;
+        /* L   */ 8'h4B: keyb_xt = 8'h26;
+        /* M   */ 8'h3A: keyb_xt = 8'h32;
+        /* N   */ 8'h31: keyb_xt = 8'h31;
+        /* O   */ 8'h44: keyb_xt = 8'h18;
+        /* P   */ 8'h4D: keyb_xt = 8'h19;
+        /* Q   */ 8'h15: keyb_xt = 8'h10;
+        /* R   */ 8'h2D: keyb_xt = 8'h13;
+        /* S   */ 8'h1B: keyb_xt = 8'h1F;
+        /* T   */ 8'h2C: keyb_xt = 8'h14;
+        /* U   */ 8'h3C: keyb_xt = 8'h16;
+        /* V   */ 8'h2A: keyb_xt = 8'h2F;
+        /* W   */ 8'h1D: keyb_xt = 8'h11;
+        /* X   */ 8'h22: keyb_xt = 8'h2D;
+        /* Y   */ 8'h35: keyb_xt = 8'h15;
+        /* Z   */ 8'h1A: keyb_xt = 8'h2C;
+        /* 0   */ 8'h45: keyb_xt = 8'h0B;
+        /* 1   */ 8'h16: keyb_xt = 8'h02;
+        /* 2   */ 8'h1E: keyb_xt = 8'h03;
+        /* 3   */ 8'h26: keyb_xt = 8'h04;
+        /* 4   */ 8'h25: keyb_xt = 8'h05;
+        /* 5   */ 8'h2E: keyb_xt = 8'h06;
+        /* 6   */ 8'h36: keyb_xt = 8'h07;
+        /* 7   */ 8'h3D: keyb_xt = 8'h08;
+        /* 8   */ 8'h3E: keyb_xt = 8'h09;
+        /* 9   */ 8'h46: keyb_xt = 8'h0A;
+        /* ~   */ 8'h0E: keyb_xt = 8'h29;
+        /* -   */ 8'h4E: keyb_xt = 8'h0C;
+        /* =   */ 8'h55: keyb_xt = 8'h0D;
+        /* \   */ 8'h5D: keyb_xt = 8'h2B;
+        /* [   */ 8'h54: keyb_xt = 8'h1A;
+        /* ]   */ 8'h5B: keyb_xt = 8'h1B;
+        /* ;   */ 8'h4C: keyb_xt = 8'h27;
+        /* '   */ 8'h52: keyb_xt = 8'h28;
+        /* ,   */ 8'h41: keyb_xt = 8'h33;
+        /* .   */ 8'h49: keyb_xt = 8'h34;
+        /* /   */ 8'h4A: keyb_xt = 8'h35;
+        /* BS  */ 8'h66: keyb_xt = 8'h0E;
+        /* SPC */ 8'h29: keyb_xt = 8'h39;
+        /* TAB */ 8'h0D: keyb_xt = 8'h0F;
+        
+        /* --- */
+        
+        default: keyb_xt = ps2_data;
         
     endcase
     
@@ -76,7 +130,7 @@ always @(posedge clock50) begin
             
             /* Запись сконвертированного из AT -> XT */
             /* Если предыдущий скан-код - это признак "отжатия" клавиши, то записать 1 в 7-й бит */
-            keyb_char   <= keyb_unpressed ? {1'b1, keyb_data_xt[6:0] } : keyb_data_xt;
+            keyb_char   <= keyb_unpressed ? {1'b1, keyb_xt[6:0] } : keyb_xt;
 
             /* Вернуть обратно в нормальное положение */
             keyb_unpressed <= 1'b0;
