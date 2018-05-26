8759cbed3a72f4521211c770e78190020d5ebf26
HFoxtail <hplus.foxtail@gmail.com>
2018-05-26 17:16:35 +0300

    almost completed...

--- projects/05_nes/alu.v
+++ projects/05_nes/alu.v
@@ -1,112 +1,80 @@
-
 module alu(
 
-    input  wire [7:0] A,        /* < Значение A */
-    input  wire [7:0] B,        /* < Значение B */
-    input  wire [3:0] alu,      /* < Режим АЛУ */
-    input  wire [7:0] P,        /* < Флаги на вход */
-    output wire [7:0] alu_res,  /* > Результат */
-    output wire [7:0] alu_flag  /* > Флаги */    
+    /* Входящие данные */
+    input  wire [7:0] A,        /* Значение A */
+    input  wire [7:0] B,        /* Значение B */
+    input  wire [3:0] ALU,      /* Режим АЛУ */
+    input  wire [7:0] P,        /* Флаги на вход */
+    input  wire [2:0] OP,       /* opcode[7:5] */
+    
+    /* Результат */
+    output wire [7:0] AR,       /* Результат */
+    output reg  [7:0] AF        /* Флаги */    
         
 );
 
-assign alu_res  = alu_rs[7:0];
-assign alu_flag = alu_fn;
+assign AR = R[7:0];
 
-// АЛУ
-// ---------------------------------------------------------------------
+/* Результат исполнения */
+reg  [8:0] R; 
 
-reg [3:0] alu_mode = 1'b0;   // Режим АЛУ
-reg [8:0] alu_rs;           // Результат АЛУ
-reg [7:0] alu_fn;           // Новые флаги
+/* Статусы ALU */
+wire Zero  = ~|R[7:0];    /* Тест на Zero */
+wire Sign  =  R[7];        /* Флаг нуля */
+wire oADC  = (A[7] ^ B[7] ^ 1'b1) & (A[7] ^ R[7]); /* Переполнение ADC */
+wire oSBC  = (A[7] ^ B[7] ^ 1'b0) & (A[7] ^ R[7]); /* Переполнение SBC */
+wire Cin   =  P[0];
+wire Carry =  R[8];
 
 always @* begin
 
-    case (alu)
+    /* Расчет результата */
+    case (ALU)
 
-        4'b0000: /* ORA */ begin
-
-            alu_rs = A | B;
-            alu_fn = {alu_rs[7], P[6:2], ~|alu_rs, P[0]};
-
-        end
-
-        4'b0001: /* AND */ begin
-
-            alu_rs = A & B;
-            alu_fn = {alu_rs[7], P[6:2], ~|alu_rs, P[0]};
-
-        end
-
-        4'b0010: /* EOR */ begin
-
-            alu_rs = A ^ B;
-            alu_fn = {alu_rs[7], P[6:2], ~|alu_rs, P[0]};
-
-        end
-
-        4'b0011: /* ADC */ begin
-
-            alu_rs = A + B + P[0];
-            alu_fn = {
-                /* S */ alu_rs[7],
-                /* V */ (A[7] ^ B[7] ^ 1'b1) & (A[7] ^ alu_rs[7]),
-                /* - */ P[5:2],
-                /* Z */ ~|alu_rs,
-                /* C */ alu_rs[8]
-            };
-
-        end
-
-        4'b0100: /* STA, STX, STY */ begin
-
-            alu_rs = A;
-            alu_fn = P;
-
-        end
-
-        4'b0101: /* LDA, LDX, LDY */ begin
-
-            alu_rs = B;
-            alu_fn = {B[7], P[6:2], ~|B, P[0]};
-
-        end
-
-        4'b0110: /* CMP, CPX, CPY */ begin
-
-            alu_rs = A - B;
-            alu_fn = {
-                /* S */ alu_rs[7],
-                /* - */ P[6:2],
-                /* Z */ ~|alu_rs,
-                /* C */ ~alu_rs[8]
-            };
-
-        end
-
-        4'b0111: /* SBC */ begin
-
-            alu_rs = A - B - (!P[0]);
-            alu_fn = {
-                /* S */ alu_rs[7],
-                /* V */ (A[7] ^ B[7]) & (A[7] ^ alu_rs[7]),
-                /* - */ P[5:2],
-                /* Z */ ~|alu_rs,
-                /* C */ ~alu_rs[8]
-            };
-
-        end
-        
-        /* ADD: Для вычислений эффективного адреса */
-        4'b1000: begin
-        
-            alu_rs = A + B;
-            alu_fn = {P[7:1], alu_rs[8]};
-        
-        end        
+        /* ORA */ 4'b0000: R = A | B;
+        /* AND */ 4'b0001: R = A & B;
+        /* EOR */ 4'b0010: R = A ^ B;
+        /* ADC */ 4'b0011: R = A + B + Cin;
+        /* STA */ 4'b0100: R = A;
+        /* LDA */ 4'b0101: R = B;
+        /* CMP */ 4'b0110: R = A - B;
+        /* SBC */ 4'b0110: R = A - B - (~Cin);
 
     endcase
+    
+    /* Расчет флагов */
+    casex (ALU) 
+        
+        /* ORA, AND, EOR, LDA, STA */
+        4'b000x, /* ORA, AND */
+        4'b0010, /* EOR */
+        4'b010x: /* STA, LDA */
+                 AF = {Sign,       P[6:2], Zero, P[0]};
+        
+        /* ADC */
+        4'b0011: AF = {Sign, oADC, P[5:2], Zero, Carry}; 
+        
+        /* CMP, SBC */       
+        4'b011x: AF = {Sign, oSBC, P[5:2], Zero, ~Carry};
+        
+        /* Сдвиговые */
+        
+        /* Флаговые */
+        4'b1100: casex (OP)
+        
+            /* CLC */ 3'b00x: AF = {P[7:1], OP[0]};
+            /* CLI */ 3'b01x: AF = {P[7:3], OP[0], P[1:0]};
+            /* CLV */ 3'b101: AF = {P[7], 1'b0, P[5:0]};
+            /* CLD */ 3'b11x: AF = {P[7:4], OP[0], P[2:0]};
+            
+        endcase
+        
+        // 4'b1101: BIT        
+        // 4'b1110: DEC
+        // 4'b1111: INC
 
+    endcase
+    
 end
 
 endmodule
--- projects/05_nes/cpu.v
+++ projects/05_nes/cpu.v
@@ -4,14 +4,13 @@ module cpu(
     input  wire        CE,      // Готовность
     output wire [15:0] ADDR,    // Адрес программы или данных
     input  wire [7:0]  DIN,     // Входящие данные
-    output wire [7:0]  DOUT,    // Исходящие данные
+    output reg  [7:0]  DOUT,    // Исходящие данные
     output reg  [15:0] EA,      // Эффективный адрес
     output reg         WREQ     // =1 Запись в память по адресу EA
 
 );
 
 assign ADDR = AM ? EA : PC;
-assign DOUT = 1'b0; /* AR[7:0] */
 
 // ---------------------------------------------------------------------
 
@@ -29,26 +28,30 @@ assign DOUT = 1'b0; /* AR[7:0] */
 // Везде переход к исполнению
 `define EXEC    5'h11
 `define IMM     5'h11
-`define REL     5'h11
 `define IMP     5'h11
 `define ACC     5'h11
 
-initial begin EA = 16'h0000; WREQ = 1'b0; end
+`define REL     5'h12 // --- отдельная тема
+
+initial begin EA = 16'h0000; WREQ = 1'b0; DOUT = 8'h00; end
 
 /* Регистры */
-reg  [7:0] A  = 8'h80;
+reg  [7:0] A  = 8'h81;
 reg  [7:0] X  = 8'hFF;
-reg  [7:0] Y  = 8'hF9;
+reg  [7:0] Y  = 8'h8F;
 reg  [7:0] S  = 8'h00;
-reg  [7:0] P  = 8'b00000000;
+reg  [7:0] P  = 8'b01000000;
 reg [15:0] PC = 16'h8000;
 
 /* Состояние процессора */
 reg         AM     = 1'b0;  /* 0=PC, 1=EA */
 reg  [4:0]  MS     = 3'h0;  /* Исполняемый цикл */
 reg  [7:0]  TR     = 3'h0;  /* Temporary Register */
+reg         WR     = 1'b0;  /* Записать в регистр RA из АЛУ */
+reg         FW     = 1'b0;  /* Записать в регистр P из АЛУ */
 reg         Cout   = 1'b0;  /* Переносы при вычислении адреса */
 reg  [7:0]  opcode = 8'h0;  /* Текущий опкод */
+reg         HOP    = 1'b0;  /* =1 Операнду требуется PC++ */
 
 /* Некоторые часто употребляемые выражения */
 wire [15:0] PCINC   = PC + 1'b1;         /* Инкремент PC */
@@ -60,56 +63,56 @@ wire [7:0]  HIDin   = DIN + Cout;        /* Перенос */
 wire [15:0] EADIN   = {DIN,   TR};
 wire [15:0] EADIH   = {HIDin, TR};
 wire        Latency = Cout | opcode[7:5] == 3'b100; /* STA или Cout */
-wire [4:0]  LATADDR = Latency ? `LAT1 : `EXEC; /* Код адреса при Latency */
+wire [4:0]  LATAD   = Latency ? `LAT1 : `EXEC; /* Код адреса при Latency */
 
 /* Исполнение микрокода */
 always @(posedge CLK) begin
 
-    case (MS) 
-    
+    case (MS)
+
         /* ИНИЦИАЛИЗАЦИЯ */
         4'h0: begin
-        
+
             casex (DIN)
 
-                8'bxxx_000_x1: begin MS <= `NDX; end // Indirect, X
+                8'bxxx_000_x1: begin MS <= `NDX; HOP <= 1'b1; end // Indirect, X
                 8'bxxx_010_x1, // Immediate
-                8'b1xx_000_x1: begin MS <= `IMM; end
-                8'bxxx_100_x1: begin MS <= `NDY; end // Indirect, Y
-                8'bxxx_110_x1: begin MS <= `ABY; end // Absolute, Y
-                8'bxxx_001_xx: begin MS <= `ZP; end  // ZeroPage
+                8'b1xx_000_x1: begin MS <= `IMM; HOP <= 1'b1; end
+                8'bxxx_100_x1: begin MS <= `NDY; HOP <= 1'b1; end // Indirect, Y
+                8'bxxx_110_x1: begin MS <= `ABY; HOP <= 1'b1; end // Absolute, Y
+                8'bxxx_001_xx: begin MS <= `ZP;  HOP <= 1'b1; end  // ZeroPage
                 8'bxxx_011_xx, // Absolute
-                8'b001_000_00: begin MS <= `ABS; end
-                8'b10x_101_1x: begin MS <= `ZPY; end // ZeroPage, Y
-                8'bxxx_101_xx: begin MS <= `ZPX; end // ZeroPage, X
-                8'b10x_111_1x: begin MS <= `ABY; end // Absolute, Y
-                8'bxxx_111_xx: begin MS <= `ABX; end // Absolute, X
-                8'bxxx_100_00: begin MS <= `REL; end // Relative
-                8'b0xx_010_10: begin MS <= `ACC; end // Accumulator
-                default: MS <= `IMP;
+                8'b001_000_00: begin MS <= `ABS; HOP <= 1'b1; end
+                8'b10x_101_1x: begin MS <= `ZPY; HOP <= 1'b1; end // ZeroPage, Y
+                8'bxxx_101_xx: begin MS <= `ZPX; HOP <= 1'b1; end // ZeroPage, X
+                8'b10x_111_1x: begin MS <= `ABY; HOP <= 1'b1; end // Absolute, Y
+                8'bxxx_111_xx: begin MS <= `ABX; HOP <= 1'b1; end // Absolute, X
+                8'bxxx_100_00: begin MS <= `REL; HOP <= 1'b1; end // Relative
+                8'b0xx_010_10: begin MS <= `ACC; HOP <= 1'b0; end // Accumulator
+                default:       begin MS <= `IMP; HOP <= 1'b0; end
 
             endcase
-            
-            PC      <= PCINC; /* PC++ */ 
+
+            PC      <= PCINC; /* PC++ */
             WREQ    <= 1'b0;  /* Отключение записи в память EA */
             opcode  <= DIN;   /* Принять новый опкод */
-            
+
         end
-        
+
         /* АДРЕСАЦИЯ */
-        
+
         /* Indirect, X */
         // -------------------------------------------------------------
-        4'h1: begin MS <= MSINC;   EA <= XDin[7:0]; AM <= 1'b1; end
-        4'h2: begin MS <= MSINC;   EA <= EAINC;     TR <= DIN; end
-        4'h3: begin MS <= `LAT1;   EA <= EADIN; end
-        
+        4'h1: begin MS <= MSINC; EA <= XDin[7:0]; AM <= 1'b1; end
+        4'h2: begin MS <= MSINC; EA <= EAINC;     TR <= DIN; end
+        4'h3: begin MS <= `LAT1; EA <= EADIN; end
+
         /* Indirect, Y */
         // -------------------------------------------------------------
-        4'h4: begin MS <= MSINC;   EA <= DIN;   AM <= 1'b1; end
-        4'h5: begin MS <= MSINC;   EA <= EAINC; TR <= YDin[7:0]; Cout <= YDin[8]; end
-        4'h6: begin MS <= LATADDR; EA <= EADIH; end
-        
+        4'h4: begin MS <= MSINC; EA <= DIN;   AM <= 1'b1; end
+        4'h5: begin MS <= MSINC; EA <= EAINC; TR <= YDin[7:0]; Cout <= YDin[8]; end
+        4'h6: begin MS <= LATAD; EA <= EADIH; end
+
         /* ZP */
         // -------------------------------------------------------------
         4'h7: begin MS <= `EXEC; EA <= DIN;       AM <= 1'b1; end
@@ -121,40 +124,152 @@ always @(posedge CLK) begin
         /* ZP,Y */
         // -------------------------------------------------------------
         4'h9: begin MS <= `LAT1; EA <= YDin[7:0]; AM <= 1'b1; end
-        
+
         /* Absolute */
         // -------------------------------------------------------------
-        4'hA: begin MS <= MSINC; TR <= DIN;     PC <= PCINC;  end
-        4'hB: begin MS <= `EXEC; EA <= EADIN;   AM <= 1'b1; end
+        4'hA: begin MS <= MSINC; TR <= DIN;    PC <= PCINC;  end
+        4'hB: begin MS <= `EXEC; EA <= EADIN;  AM <= 1'b1; end
 
         /* Absolute,X */
         // -------------------------------------------------------------
-        4'hC: begin MS <= MSINC;   PC <= PCINC; TR <= XDin[7:0]; Cout <= XDin[8]; end
-        4'hD: begin MS <= LATADDR; EA <= EADIH; AM <= 1'b1;end
+        4'hC: begin MS <= MSINC; TR <= XDin[7:0]; PC <= PCINC; Cout <= XDin[8]; end
+        4'hD: begin MS <= LATAD; EA <= EADIH;     AM <= 1'b1;end
 
         /* Absolute,Y */
         // -------------------------------------------------------------
-        4'hE: begin MS <= MSINC;   PC <= PCINC; TR <= YDin[7:0]; Cout <= YDin[8]; end
-        4'hF: begin MS <= LATADDR; EA <= EADIH; AM <= 1'b1; end
-        
+        4'hE: begin MS <= MSINC; TR <= YDin[7:0]; PC <= PCINC; Cout <= YDin[8]; end
+        4'hF: begin MS <= LATAD; EA <= EADIH;     AM <= 1'b1; end
+
         /* Отложенный такт (для адресации) */
         `LAT1: MS <= `EXEC;
-        
+
         /* Исполнение инструкции */
         // -------------------------------------------------------------
-        
+
         /* Исполнение инструкции */
-        `EXEC: begin        
-        
-            PC <= PCINC; /* Инкремент PC по завершении разбора адреса */
-    
-            MS <= 1'b0; // ! временно !
-        
+        `EXEC: begin
+
+            /* Инкремент PC по завершении разбора адреса */
+            if (HOP) PC <= PCINC; 
+
+            casex (opcode)
+
+                /* STA/STY/STX для АЛУ */
+                8'b100_xxx_01, 
+                8'b100_xx1_x0: {AM, MS, WREQ, DOUT} <= {3'b001, AR[7:0]};
+
+                // По умолчанию, завершение инструкции
+                default: {AM, MS} <= 2'b00;
+
+            endcase
+
+        end
+
+        /* Исполнение инструкции B<cc> */
+        `REL: begin
+
+
         end
 
     endcase
-    
+
 end
+
+/* Подготовка данных на этапах исполнения */
+// ---------------------------------------------------------------------
+
+always @* begin
+
+    alu = {1'b0, opcode[7:5]}; /* По умолчанию */
+    RA  = 2'b00; /* A */
+    RB  = 2'b00; /* DIN */
+    WR  = 1'b0;
+    FW  = 1'b0;
+
+    if (MS == `EXEC) casex (opcode)
+
+        /* STA, CMP */
+        8'b1x0_xxx_01: FW = opcode[6];
+
+        /* ADC, SBC, AND, ORA, EOR, LDA */
+        8'bxxx_xxx_01: {WR, FW} = 2'b11;
+
+        /* STY, STX */
+        8'b100_xx1_00: {RA} = 2'b10; // Y
+        8'b100_xx1_10: {RA} = 2'b01; // X
+        /* LDY */
+        8'b101_xx1_00,
+        8'b101_000_00: {WR, RA, FW} = 4'b1_10_1;
+        /* LDX */
+        8'b101_xx1_10,
+        8'b101_000_10: {WR, RA, FW} = 4'b1_01_1;
+        /* CPY */
+        8'b110_xx1_00,
+        8'b110_000_00: {RA, FW} = 3'b10_1;
+        /* CPX */
+        8'b111_xx1_00,
+        8'b111_000_00: {alu, RA, FW} = 7'b0110_01_1;
+        /* TYA */
+        8'b100_110_00: {alu, RB, FW, WR} = 8'b0101_10_11;
+        /* CLC, SEC, CLI, SEI, CLV, CLD, SED */
+        8'bxxx_110_00: {alu, FW} = 5'b1100_1;
+
+    endcase
+
+end
+
+/* Запись результата */
+// ---------------------------------------------------------------------
+
+always @(posedge CLK) begin
+
+    /* Писать результат АЛУ */
+    if (WR) case (RA)
+        2'b00: A <= AR;
+        2'b01: X <= AR;
+        2'b10: Y <= AR;
+    endcase
+
+    /* Писать флаги */
+    if (FW) P <= AF;
+
+end
+
+/* Вычислительное устройство */
+// ---------------------------------------------------------------------
+
+reg [3:0] alu = 4'h0;
+reg [1:0] RA = 2'b00;
+reg [1:0] RB = 2'b00;
+reg [7:0] Ax;
+reg [7:0] Bx;
+reg [7:0] AR;
+reg [7:0] AF;
+
+always @* begin
+
+    case (RA)
+        2'b00: Ax = A;
+        2'b01: Ax = X;
+        2'b10: Ax = Y;
+        2'b11: Ax = 0;
+    endcase
+
+    case (RB)
+        2'b00: Bx = DIN;
+        2'b01: Bx = X;
+        2'b10: Bx = Y;
+        2'b11: Bx = S;
+    endcase
+
+end
+
+alu ALU(
+
+    /* Вход */  Ax, Bx, alu, P, opcode[7:5],
+    /* Выход */ AR, AF
+
+);
 
 endmodule
 
