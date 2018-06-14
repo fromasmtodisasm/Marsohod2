1e7f62424eb24a5f82ff35c0fb4e752f0353f23a
HFoxtail <hplus.foxtail@gmail.com>
2018-06-14 16:29:43 +0300

    fix

--- projects/05_nes/tests/test1/loderunner.sh
+++ projects/05_nes/tests/test1/loderunner.sh
@@ -8,5 +8,20 @@ dd if=loderunner.nes of=rom.bin skip=16400 bs=1 seek=32768 count=8192 conv=notru
 #mode com8 baud=460800 data=8
 #copy rom.bin /b com8 
 
-stty -F /dev/ttyUSB1 460800 cs8 -cstopb -parenb raw
+# Состояние устройства COM8:
+# http://bsvi.ru/signaly-kvitirovaniya-rts-cts-itp-i-rs232-voobshhe/
+# ---------------------------
+#    Скорость:              460800
+#    Четность:              Even
+#    Биты данных:           8
+#    Стоповые биты:         1
+#    Таймаут:               OFF
+#    XON/XOFF:              OFF
+#    Синхронизация CTS:     OFF
+#    Синхронизация DSR:     OFF
+#    Чувствительность DSR:  OFF
+#    Цепь DTR:              ON
+#    Цепь RTS:              ON
+
+stty -F /dev/ttyUSB1 460800 cs8 -cstopb -parenb raw -ixoff
 cat rom.bin > /dev/ttyUSB1
