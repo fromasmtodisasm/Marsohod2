aac2e346ad9a6d5182fd91e175e651d43f347426
HFoxtail <hplus.foxtail@gmail.com>
2018-06-24 13:50:11 +0300

    fin 8086 instr table

--- projects/09_8632/ia32/bios.asm
+++ projects/09_8632/ia32/bios.asm
@@ -1,12 +1,4 @@
 
         org     0
-        inc     byte [bx + si]
-        dec     word [$0022]
-        call    word [bx]
-        call    eax
-        call    dword [bx]
-        jmp     bx
-        jmp     ebx
-        jmp     dword [bx]
-        push    word [bx]
-        push    dword [bx]
+        mov     ax, bx
+        cmp     bx, cx
